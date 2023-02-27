339fbff4dbca76a639c7765965c93e13fe37897b
ob <oberezhinskiy@gmail.com>
2023-02-27 12:58:25 +0300

    4

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import bodyParser from 'body-parser';
-import express from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: true,
   });
-  app.use(express.urlencoded({ extended: true }));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
